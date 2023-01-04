47bafea7607fd65819f753f19211ef51b41d030b
Jelenarog <jelena.rogic90@gmail.com>
2023-01-03 18:03:16 -0700

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 //const fs = require('fs');
 const api = require('./routes/notes');
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
